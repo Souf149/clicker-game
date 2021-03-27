ad354b553d95e25a9a6927ef8755eb93b162722c
Soufyan <MetaByte149@gmail.com>
2021-03-27 11:44:51 +0100

    Added scroll views to get the shop. TODO: work on using anchors instead of VBOX/HBOX

--- stages/HUD.gd
+++ stages/HUD.gd
@@ -1,16 +1,16 @@
-extends CanvasLayer
+extends Control
 
 signal skillButtonPressed(skillNumber)
 
 const DEFAULT_TEXT = "DPS: "
 
 func _ready():
-	$Label.text = DEFAULT_TEXT + str(0)
+	$Panel/Label.text = DEFAULT_TEXT + str(0)
 
 
 
 func _on_DemonHunter_newDps(dps):
-	$Label.text = DEFAULT_TEXT + str(dps)
+	$Panel/Label.text = DEFAULT_TEXT + str(dps)
 
 func skillButtonPressed(skillNumber):
 	emit_signal("skillButtonPressed", skillNumber)
