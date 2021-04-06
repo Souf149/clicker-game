7083dd34f8f6e6881c16c108c95f6b78c54d9e20
Soufyan <MetaByte149@gmail.com>
2021-04-06 08:47:43 +0200

    temp push (adding enemy)

--- stages/world.gd
+++ stages/world.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+var slimeEnemy = preload("res://enemies/Slime/Slime.tscn")
+var screenSize
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	screenSize = get_viewport_rect().size
+	print(screenSize)
+	var slime = Global.instance_node(slimeEnemy, Vector2(screenSize, screenSize.y*0.5), self)
+	print(slime.global_position)
+	
+	
